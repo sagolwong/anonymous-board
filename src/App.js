499fc1c99e34dc4eadbd564607a1543f1d6f8639
sagolwong <5910110678@psu.ac.th>
2019-04-27 18:08:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// App.js
+import React, { Component } from 'react';
+import Header from '../src/components/Header';
+import MessageList from './components/MessageList';
+import MessageBox from './components/MessageBox';
+import firebase from 'firebase';
+class App extends Component {
+  constructor(props){
+  super(props);
+  var config = {
+    apiKey: "AIzaSyBV192Df9q_x2POf70aNheD2Kg4VMjKeZ0",
+    authDomain: "react-encrypt-board.firebaseapp.com",
+    databaseURL: "https://react-encrypt-board.firebaseio.com",
+    projectId: "react-encrypt-board",
+    storageBucket: "react-encrypt-board.appspot.com",
+    messagingSenderId: "421884945197"
+  };
+  firebase.initializeApp(config);
+}
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Encrypt Board" />
+      <div className="columns">
+        <div className="column is-3"></div>
+        <div className="column is-6">
+          <MessageBox db={firebase} />
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column is-3"></div>
+        <div className="column is-6">
+          <MessageList db={firebase} />
+        </div>
+      </div>
     </div>
   );
+ }
 }
-
 export default App;
